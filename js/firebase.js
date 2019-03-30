fdede7f9ab078d574f1cc71aad6c428b6047fca0
Guilherme de Freitas Reis <guilhermereis9876@gmail.com>
2019-03-29 21:35:10 -0300

    Initial commit

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCh5ttZfIcVpRtE0WkzwQ3lmkCjHVN4tsc",
+    authDomain: "diaristas0.firebaseapp.com",
+    databaseURL: "https://diaristas0.firebaseio.com",
+    projectId: "diaristas0",
+    storageBucket: "diaristas0.appspot.com",
+    messagingSenderId: "902558032865"
+  };
+  firebase.initializeApp(config);
